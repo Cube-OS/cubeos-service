45d276f1b23e155b7cbcf70c6840d2c2ed3385c7
Patrick Oppel <patrick.oppel94@gmail.com>
2024-04-19 17:43:59 +1000

    Flight

--- src/app/app_macro.rs
+++ src/app/app_macro.rs
@@ -1,6 +1,7 @@
 #[macro_export]
 macro_rules! app_macro{
     (
+        $app: tt: $timeout: tt;
         $service: tt: $struct: tt {
             $(            
                 $(query)?$(mutation)?: $type: ident => fn $func: tt (&$(mut )?self $(,$msg: tt: $cmd: ty)*) -> $ign1: tt<$rep: ty> $(; out: $gql_q: ty)?;
@@ -10,6 +11,7 @@ macro_rules! app_macro{
         use cubeos_service::udp_rs::Connection;
         use cubeos_service::command_id;
         use std::str::FromStr;
+        use log::debug;
 
         command_id!{
             $($type,)*
@@ -20,8 +22,14 @@ macro_rules! app_macro{
                 Config::new(&stringify!($service).replace("_","-"))
                     .unwrap()
                     .hosturl()
-                    .unwrap()                    
+                    .unwrap()                  
             };
+            // static ref APP_URL: String = {
+            //     Config::new(&stringify!($app).replace("_","-"))
+            //         .unwrap()
+            //         .hosturl()
+            //         .unwrap()                  
+            // };
         }
 
         pub struct $struct {}
@@ -29,11 +37,25 @@ macro_rules! app_macro{
             $(
                 pub fn $func($($msg:$cmd),*) -> Result<$rep> {
                     let app_url = "0.0.0.0:0".to_string();
+                    // let app_url = APP_URL.to_string();
                     let connection = Connection::from_path(app_url,HOST_URL.to_string());
-                    match Command::<CommandID,$rep>::parse(&connection.transfer_timeout(Command::serialize(CommandID::$type,($($msg),*))?,std::time::Duration::from_secs(1))?) {
-                        Ok(c) => Ok(c.data),
-                        Err(e) => Err(e),
-                    }                
+                    let mut command = Command::serialize(CommandID::$type,($($msg),*))?;
+                    // command.insert(0,0);
+                    debug!("Command: {:?}", command);
+                    match connection.transfer_timeout(command,std::time::Duration::from_secs($timeout)) {
+                        Ok(response) => {
+                            debug!("Response: {:?}", response);
+                            match Command::<CommandID,$rep>::parse(&response) {
+                                Ok(c) => Ok(c.data),
+                                Err(e) => Err(e.into()),
+                            }
+                        },
+                        Err(e) => Err(e.into()),
+                    }
+                    // match Command::<CommandID,$rep>::parse(&connection.transfer_timeout(command,std::time::Duration::from_secs(1))?) {
+                    //     Ok(c) => Ok(c.data),
+                    //     Err(e) => Err(e),
+                    // }                
                 }
             )*
         }       
